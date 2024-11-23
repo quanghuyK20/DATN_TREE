f798dbd935d3602a98e93e86cddee00ccd04e75a
Quang Huy <quanghuy@192.168.1.100.non-exists.ptr.local>
2024-11-23 20:21:34 +0700

    set up docker

---
+++ socket/index.js
@@ -0,0 +1,63 @@
+const io = require("socket.io")(8900, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ['GET', 'POST'],
+    credentials: true,
+    secure: true,
+    transports: ['websocket', 'polling'],
+  },
+  allowEIO4: true,
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  console.log(users.find((user) => user.userId === userId));
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  //when ceonnect
+  console.log("a user connected.");
+  console.log(socket.id);
+  const socketID = socket.id
+  //take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    console.log("line 35",userId);
+    addUser(userId, socketID);
+    console.log('new users', users);
+    io.emit("getUsers", users);
+  });
+
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    console.log("users line 43",users)
+    console.log(receiverId);
+    debugger
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+console.log('====================================');
+console.log(receiverId);
+console.log('====================================');
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
